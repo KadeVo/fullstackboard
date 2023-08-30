7fb4afa7a8f326f25d5ff804f442667da0ea1c74
Kadin Vo <kadinvo@gmail.com>
2023-08-30 16:46:42 +1200

    Added migrations

---
+++ server/db/migrations/20230830042956_create_jobs_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('jobs', (table) => {
+        table.increments('id').primary();
+        table.string('title').notNullable();
+        table.string('location');
+        table.text('description');
+        table.integer('company_id').unsigned().references('id').inTable('companies');
+        table.timestamps(true, true);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable('jobs')
+};
---
+++ server/db/migrations/20230830043155_create_applications_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable('applications', (table) => {
+        table.increments('id').primary();
+        table.integer('job_id').unsigned().references('id').inTable('jobs');
+        table.integer('user_id').unsigned().references('id').inTable('users');
+        table.timestamps(true, true);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable('applications')
+};
