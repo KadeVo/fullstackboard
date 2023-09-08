c58718ba11b093d34e2639b3e3b1047cca1cf632
Kadin Vo <kadinvo@gmail.com>
2023-09-08 15:22:34 +1200

    test

--- server/db/seeds/2users_seed.js
+++ server/db/seeds/2users_seed.js
@@ -7,12 +7,10 @@ export async function seed(knex) {
     {
       username: 'user1',
       email: 'user1@example.com'
-      // password: 'password1',
     },
     {
       username: 'user2',
       email: 'user2@example.com'
-      // password: 'password2',
     },
 
   ]);
