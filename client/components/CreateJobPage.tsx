e47eaee1fdc1ba766b2e448e5cc3f95bb7d57758
Kadin Vo <kadinvo@gmail.com>
2023-09-28 15:36:01 +1300

    Added style to the create job page

--- client/components/CreateJobPage.tsx
+++ client/components/CreateJobPage.tsx
@@ -1,12 +1,14 @@
-import AddJobForm from './AddJob.tsx';
+import AddJobForm from './AddJob.tsx'
 
 function CreateJobPage() {
-    return (
-        <div>
-            <h1>Create a New Job Listing</h1>
-            <AddJobForm />
-        </div>
-    );
+  return (
+    <div className="container mx-auto p-4 text-center">
+      <h1 className="text-3xl font-semibold mb-4">
+        Create your listing today!
+      </h1>
+      <AddJobForm />
+    </div>
+  )
 }
 
-export default CreateJobPage;
+export default CreateJobPage
