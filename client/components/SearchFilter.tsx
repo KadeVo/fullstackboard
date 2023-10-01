487a7f6abee7dc7e583443ba9a22d0e38b1de633
Kadin Vo <kadinvo@gmail.com>
2023-10-02 11:26:11 +1300

    Restyled search to be more appealing

--- client/components/SearchFilter.tsx
+++ client/components/SearchFilter.tsx
@@ -1,6 +1,6 @@
 function SearchFilter() {
   return (
-    <div className="bg-gray-200 p-4 rounded-md">
+    <div className="bg-gray-200 p-4 rounded-md text-center">
       <div className="flex items-center justify-center mb-4">
         <img
           src="path/to/your/image.jpg"
@@ -8,24 +8,26 @@ function SearchFilter() {
           className="max-w-full max-h-40"
         />
       </div>
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col items-center space-y-4">
         <label htmlFor="search" className="text-red-600 text-lg">
           Search for jobs:
         </label>
-        <input
-          type="text"
-          id="search"
-          placeholder="Enter keywords here"
-          className="border border-gray-300 rounded-md px-4 py-2"
-        />
+        <div className="flex items-center space-x-2">
+          <input
+            type="text"
+            id="search"
+            placeholder="Enter keywords here"
+            className="border border-gray-300 rounded-md px-4 py-2"
+          />
+          <button
+            type="button"
+            className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-3 rounded-md"
+          >
+            Search
+          </button>
+        </div>
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
-        <button
-          type="button"
-          className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md"
-        >
-          Search
-        </button>
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-x-4 md:items-center">
         <label htmlFor="location" className="text-lg">
           Location:
         </label>
