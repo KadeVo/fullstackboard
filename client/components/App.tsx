ae073f13c9ca88ea6b7a19bfe606146b7d40519e
Kadin Vo <kadinvo@gmail.com>
2023-10-24 17:22:46 +1300

    adjusted router and nav function

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,20 @@
 import { Link, Outlet } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 function App() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  const handleSignOut = () => {
+    console.log('sign out')
+    logout()
+  }
+
+  const handleSignIn = () => {
+    console.log('sign in')
+    loginWithRedirect()
+  }
+
   return (
     <div>
       <nav className="bg-gray-800 py-4">
@@ -17,6 +31,13 @@ function App() {
               </Link>
             </li>
           </ul>
+          <IfAuthenticated>
+            <button onClick={handleSignOut}>Sign out</button>
+            {user && <p>Signed in as: {user?.nickname}</p>}
+          </IfAuthenticated>
+          <IfNotAuthenticated>
+            <button onClick={handleSignIn}>Sign in</button>
+          </IfNotAuthenticated>
         </div>
       </nav>
       <main className="container mx-auto p-4">
--- client/index.tsx
+++ client/index.tsx
@@ -3,15 +3,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <Auth0Provider
+      domain="https://mako-kadin.au.auth0.com"
+      clientId="DbVHrwGs9kKbWNQZxi8lupQkhLpAf8Yz"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Auth0Provider>
   )
 })
-
